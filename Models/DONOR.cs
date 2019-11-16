9a442e24af54df66201c8bad3d4821902c0005b8
NoobScorpio <44445634+NoobScorpio@users.noreply.github.com>
2019-11-17 01:42:14 +0500

    Add files via upload

---
+++ Models/DONOR.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodB.Models
+{
+    public class DONOR
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string BloodGroup { get; set; }
+    }
+}
